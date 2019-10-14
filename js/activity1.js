e192a742085d1d888c9d3ee76bd52a6bc6d3537c
Helen Nazarenko <nazarenh@bc.edu>
2019-10-14 01:39:47 -0400

    edit

--- js/activity1.js
+++ js/activity1.js
@@ -13,9 +13,79 @@ let svg = d3.select("#chart-area").append("svg")
 // Load data
 d3.json("data/airports.json")
 .then(function(data) {
+  console.log(data);
+
+  let force = d3.forceSimulation(data.nodes)
+    .force("charge", d3.forceManyBody().strength(-10))
+    .force("link", d3.forceLink(data.links).distance(30))
+    .force("center", d3.forceCenter().x(width/2).y(height/2))
+    .force("collide", d3.forceCollide().radius(15).strength(5))
+    .force("manyBody", d3.forceManyBody().strength(30))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
   
   // 2a) DEFINE 'NODES' AND 'EDGES'
+    // Draw nodes
+    let node = svg.selectAll(".node")
+      .data(data.nodes)
+      .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 10)
+      .attr("fill", function(d){
+        if (d.country == "United States"){return "blue";}
+        else {return "red";}
+      });
+
+    //Create edges as lines
+    var edges = svg.selectAll("line")
+      .data(data.links)
+      .enter()
+      .append("line")
+      .style("stroke", "#ccc")
+      .style("stroke-width", 1);
+
+      force.on("tick", function() {
+		
+        // Update node coordinates
+        node
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+        
+        // Update edge coordinates
+        edges
+            .attr("x1", function(d){return d.source.x;})
+            .attr("x2", function(d){return d.target.x;})
+            .attr("y1", function(d){return d.source.y;})
+            .attr("y2", function(d){return d.target.y;})
+    });
+
+    //adding a tooltip:
+    node.append("title")
+      .text(function(d) {
+          return d.name;
+      });
   // 2b) START RUNNING THE SIMULATION
+function dragStarted(d) {
+    if (!d3.event.active) force.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragging(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function dragEnded(d) {
+    if (!d3.event.active) force.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+}
+
+  node.call(d3.drag()
+    .on("start", dragStarted)
+    .on("drag", dragging)
+    .on("end", dragEnded));
 
   // 3) DRAW THE LINKS (SVG LINE)
 
--- js/activity2.js
+++ js/activity2.js
@@ -1,32 +1,104 @@
 
-let width = 1000,
-height = 600;
+let width = 1200,
+height = 650;
 
 let svg = d3.select("#chart-area").append("svg")
 .attr("width", width)
 .attr("height", height);
 
 // Load data
+Promise.all([ // load multiple files
+	d3.json('data/world-110m.json'),
+	d3.json('data/airports.json')
+]).then(data=>{
+	let data1 = data[0]; // data1.json
+	let data2 = data[1]; // data2.json
 
-// Load shapes of U.S. counties (TopoJSON)
-d3.json("data/us-10m.json")
-.then(function(data) {
-    let projection = d3.geoAlbersUsa()
-    .translate([width / 2, height / 2]);
-
-    let path = d3.geoPath()
-    .projection(projection);
+	createVisualization(data1, data2)
+})
 
 
-	// Convert TopoJSON to GeoJSON (target object = 'states')
-	let usa = topojson.feature(data, data.objects.states).features
-	
+function createVisualization(data1, data2) {
+	// Visualize data1 and data2
+	console.log(data1);
+	let projection = d3.geoConicEqualArea()
+		.translate([width / 2, height / 2])
+		.scale([210])
+	let path = d3.geoPath()
+		.projection(projection);
+		// Convert TopoJSON to GeoJSON (target object = 'states')
+	let world = topojson.feature(data1, data1.objects.countries).features
+
 	// Render the U.S. by using the path generator
 	svg.selectAll("path")
-			.data(usa)
+			.data(world)
 		.enter().append("path")
-			.attr("d", path);
-});
+			.attr("d", path)
+			.style("fill", "orange")
+
+	console.log(data2);
+			
+
+	let node = svg.selectAll(".node")
+      .data(data2.nodes)
+      .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 5)
+	  .attr("fill", "purple")
+	  .attr("transform", function(d) {
+		return "translate(" + projection([d.longitude, d.latitude]) + ")";
+	})
+	  .append('title')
+	  .text(function(d){return d.name;})
+
+
+	var edges = svg.selectAll("line")
+		.data(data2.links)
+		.enter()
+		.append("line")
+		.style("stroke", "#ccc")
+		.style("stroke-width", 1);
+
+
+	edges
+		.attr("x1", function(d){
+			return projection( [data2.nodes[d.source].longitude ,data2.nodes[d.source].latitude] )[0];
+		})
+		.attr("y1",function(d){
+			return projection( [data2.nodes[d.source].longitude, data2.nodes[d.source].latitude])[1];
+		})
+		.attr("x2", function(d){
+			return projection( [data2.nodes[d.target].longitude, data2.nodes[d.target].latitude])[0];
+		})
+		.attr("y2",function(d){
+			return projection( [data2.nodes[d.target].longitude, data2.nodes[d.target].latitude])[1];
+		})
+}
+
+
+
+// Load shapes of U.S. counties (TopoJSON)
+// d3.json("data/us-10m.json")
+// .then(function(data) {
+//     let projection = d3.geoAlbersUsa()
+//     .translate([width / 2, height / 2]);
+
+//     let path = d3.geoPath()
+//     .projection(projection);
+
+
+// 	// Convert TopoJSON to GeoJSON (target object = 'states')
+// 	let usa = topojson.feature(data, data.objects.states).features
+	
+// 	// Render the U.S. by using the path generator
+	// svg.selectAll("path")
+	// 		.data(usa)
+	// 	.enter().append("path")
+	// 		.attr("d", path);
+// });
+
+
+
 
 
 // 2a) DEFINE 'NODES' AND 'EDGES'
